41facf4668cc8cfe22105c16f0e3dc1d79cb8b67
Christopher R. Wicks <wickstopher@gmail.com>
2017-10-03 21:43:06 -0400

    Logger transformations and timestamp logging.

--- src/Stomp/TLogger.hs
+++ src/Stomp/TLogger.hs
@@ -3,25 +3,42 @@ module Stomp.TLogger where
 import System.IO
 import Control.Concurrent
 import Control.Concurrent.TxEvent
+import Data.Time
 
-data Logger = Logger Handle (SChan String) ThreadId
+data Logger = Logger Handle (SChan String) ThreadId (IO String -> IO String)
 
 initLogger :: Handle -> IO Logger
 initLogger handle = do
     logChannel <- sync newSChan
     tid <- forkIO $ logLoop handle logChannel
-    return $ Logger handle logChannel tid
+    return $ Logger handle logChannel tid id
+
+dateTimeLogger :: Handle -> IO Logger
+dateTimeLogger handle = do
+    logger <- initLogger handle
+    return $ addTransform timeStampTransform logger
 
 log :: Logger -> String -> IO ()
-log (Logger handle logChannel _) message = do
-    sync $ sendEvt logChannel $ message ++ "\n"
+log (Logger handle logChannel _ f) message = do
+    message' <- f (return message)
+    sync $ sendEvt logChannel $ message' ++ "\n"
 
 prompt :: Logger -> String -> IO ()
-prompt (Logger handle logChannel _) message = do
-    sync $ sendEvt logChannel message
+prompt (Logger handle logChannel _ f) message = do
+    message' <- f (return message)
+    sync $ sendEvt logChannel $ message'
 
 logLoop :: Handle -> (SChan String) -> IO ()
 logLoop handle logChannel = do
     message <- sync $ recvEvt logChannel
     hPutStr handle message
     logLoop handle logChannel
+
+addTransform :: (IO String -> IO String) -> Logger -> Logger
+addTransform f' (Logger h c t f) = Logger h c t (f . f')
+
+timeStampTransform :: IO String -> IO String
+timeStampTransform s = do
+    time <- getCurrentTime
+    s' <- s
+    return $ "[" ++ (show time) ++ "] " ++ s'
