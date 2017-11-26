140dd5c89eb0eba980d9fe1541cbdcd756e4a0ea
Christopher R. Wicks <wickstopher@gmail.com>
2017-11-26 14:14:22 -0500

    Add current state function for Incrementer; looping event

--- src/Stomp/Increment.hs
+++ src/Stomp/Increment.hs
@@ -3,21 +3,26 @@ module Stomp.Increment (
     Incrementer,
     newIncrementer,
     getNext,
-    getNextEvt
+    getNextEvt,
+    getLast,
+    getLastEvt
 ) where
 
 import Control.Concurrent
 import Control.Concurrent.TxEvent
 
 -- |An Incrementer maintains the state of an Integer that increases by one with each access.
-data Incrementer = Incrementer (SChan Integer)
+data Incrementer = Incrementer (SChan Integer) (SChan Request)
+
+data Request     = NextValue | CurrentValue
 
 -- |Initialize a new incrementer and return it in an IO context. The first value retrieved will be 0.
 newIncrementer :: IO Incrementer
 newIncrementer = do
-    channel <- sync newSChan
-    forkIO $ incrementLoop 0 channel
-    return $ Incrementer channel
+    intChan <- sync newSChan
+    reqChan <- sync newSChan
+    forkIO $ incrementLoop 0 intChan reqChan 
+    return $ Incrementer intChan reqChan
 
 -- |Get the next sequential Integer and return it in an IO context.
 getNext :: Incrementer -> IO Integer
@@ -25,11 +30,34 @@ getNext incrementer = sync $ getNextEvt incrementer
 
 -- |Get the next sequential Integer and return it in an Evt context.
 getNextEvt :: Incrementer -> Evt Integer
-getNextEvt (Incrementer channel) = recvEvt channel
+getNextEvt (Incrementer intChan reqChan) = do
+    sendEvt reqChan NextValue
+    recvEvt intChan
+
+-- |Get the most recent value returned by the Incrementer and return it in an IO context.
+getLast :: Incrementer -> IO Integer
+getLast incrementer = sync $ getLastEvt incrementer
+
+-- |Get the most recent value returned by Incrementer and return it in an Evt context.
+getLastEvt :: Incrementer -> Evt Integer
+getLastEvt (Incrementer intChan reqChan) = do
+    sendEvt reqChan CurrentValue
+    recvEvt intChan
 
 -- |State management for the Incrementer; blocks until it receives a corresponding recvEvt on the Integer
 -- channel, send its current value, and then increments its value.
-incrementLoop :: Integer -> SChan Integer -> IO ()
-incrementLoop value channel = do
-    sync $ sendEvt channel value
-    incrementLoop (value + 1) channel
+incrementLoop :: Integer -> SChan Integer -> SChan Request -> IO ()
+incrementLoop value intChan reqChan = do
+    value' <- sync $ incrementEvt value intChan reqChan
+    incrementLoop value' intChan reqChan
+
+-- |Looping event to handle multiple consecutive requests in the same synchronization.
+incrementEvt :: Integer -> SChan Integer -> SChan Request -> Evt Integer
+incrementEvt value intChan reqChan = do
+    request <- recvEvt reqChan
+    let value' = case request of
+            NextValue    -> (value + 1)
+            CurrentValue -> value
+        in do
+            sendEvt intChan value'
+            (alwaysEvt value') `chooseEvt` (incrementEvt value' intChan reqChan)
